77bd3c98d8cc31061ba690cf38e139f02fd671a3
guokuo <guokuo@ny.renderg.com>
2026-05-28 10:30:21 +0800

    feat: add WeChat HPC system config options

--- controller/misc.go
+++ controller/misc.go
@@ -69,6 +69,7 @@ func GetStatus(c *gin.Context) {
 		"wechat_login":                common.WeChatAuthEnabled,
 		"wechat_app_id":               common.WeChatAppID,
 		"wechat_receive_token":        common.WeChatReceiveToken,
+		"wechat_hpc_server_address":   common.WeChatHpcServerAddress,
 		"server_address":              system_setting.ServerAddress,
 		"turnstile_check":             common.TurnstileCheckEnabled,
 		"turnstile_site_key":          common.TurnstileSiteKey,
